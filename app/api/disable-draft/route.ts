28b8d6981ceb2bd911abfe11d18b2310603719a8
pavlo-ol <pavlo.oliinyk@valtech.com>
2026-03-19 19:06:06 +0100

    feat: implement draft mode handling with secret validation and redirection

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,6 +1,7 @@
 import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function GET(request: Request) {
   (await draftMode()).disable();
-  return new Response("Draft mode is disabled");
+  redirect("/");
 }
--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,2 +1,20 @@
-//@ts-ignore
-export { enableDraftHandler as GET } from "@contentful/vercel-nextjs-toolkit/app-router";
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+  const slug = searchParams.get("slug");
+
+  // Check the secret
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
+    return new Response("Invalid token", { status: 401 });
+  }
+
+  // Enable Draft Mode
+  (await draftMode()).enable();
+
+  // Redirect to the path from the fetched post
+  redirect(slug || "/");
+}
